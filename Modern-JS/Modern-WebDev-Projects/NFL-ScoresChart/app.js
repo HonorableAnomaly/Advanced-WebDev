ebea7b5756f18ef3c652ee19448d48a15baf9782
Ricky Argenbright <honorable.anomaly@gmail.com>
2022-08-13 15:46:21 -0700

    Refactor scores chart
    app.js
    - comment out first example
    +factory function makeChart wrapping prior code and refactors
    +isWinner and getScoreLine functions wrapping prior code and refactors
    +new variable calls to create and append charts based on params
    
    index.html
    +2 sections with unique id's and h2's for charts to be appended to

--- Modern-JS/Modern-WebDev-Projects/NFL-ScoresChart/app.js
+++ Modern-JS/Modern-WebDev-Projects/NFL-ScoresChart/app.js
@@ -85,10 +85,49 @@ const vikingsGames = [
   },
 ];
 
-const ulParent = document.createElement("ul");
-for (let game of vikingsGames) {
-  const { homeTeam, awayTeam } = game;
-  const gameLi = document.createElement("li");
+// First example
+
+// const ulParent = document.createElement("ul");
+// for (let game of vikingsGames) {
+//   const { homeTeam, awayTeam } = game;
+//   const gameLi = document.createElement("li");
+//   const { team: hTeam, points: hPoints } = homeTeam;
+//   const { team: aTeam, points: aPoints } = awayTeam;
+//   const teamNames = `${aTeam} @ ${hTeam}`;
+//   let scoreLine;
+//   if (aPoints > hPoints) {
+//     scoreLine = `<b>${aPoints}</b>-${hPoints}`;
+//   } else {
+//     scoreLine = `${aPoints}-<b>${hPoints}</b>`;
+//   }
+//   const vikings = hTeam === "Minnesota" ? homeTeam : awayTeam;
+//   gameLi.classList.add(vikings.isWinner ? "win" : "loss");
+//   gameLi.innerHTML = `${teamNames} ${scoreLine}`;
+//   ulParent.append(gameLi);
+// }
+
+// document.body.prepend(ulParent);
+
+//
+// Refactored for either team, or any data
+const makeChart = (games, targetTeam) => {
+  const ulParent = document.createElement("ul");
+  for (let game of games) {
+    const { homeTeam, awayTeam } = game;
+    const gameLi = document.createElement("li");
+    gameLi.innerHTML = getScoreLine(game);
+    gameLi.classList.add(isWinner(game, targetTeam) ? "win" : "loss");
+    ulParent.append(gameLi);
+  }
+  return ulParent;
+};
+
+const isWinner = ({ homeTeam, awayTeam }, targetTeam) => {
+  const target = homeTeam.team === targetTeam ? homeTeam : awayTeam;
+  return target.isWinner;
+};
+
+const getScoreLine = ({ homeTeam, awayTeam }) => {
   const { team: hTeam, points: hPoints } = homeTeam;
   const { team: aTeam, points: aPoints } = awayTeam;
   const teamNames = `${aTeam} @ ${hTeam}`;
@@ -98,10 +137,13 @@ for (let game of vikingsGames) {
   } else {
     scoreLine = `${aPoints}-<b>${hPoints}</b>`;
   }
-  const vikings = hTeam === "Minnesota" ? homeTeam : awayTeam;
-  gameLi.classList.add(vikings.isWinner ? "win" : "loss");
-  gameLi.innerHTML = `${teamNames} ${scoreLine}`;
-  ulParent.append(gameLi);
-}
+  return `${teamNames} ${scoreLine}`;
+};
 
-document.body.prepend(ulParent);
+const mvSection = document.querySelector("#mv");
+const gbpSection = document.querySelector("#gbp");
+
+const mvChart = makeChart(vikingsGames, "Minnesota");
+const gbpChart = makeChart(vikingsGames, "Green Bay");
+mvSection.appendChild(mvChart);
+gbpSection.appendChild(gbpChart);
