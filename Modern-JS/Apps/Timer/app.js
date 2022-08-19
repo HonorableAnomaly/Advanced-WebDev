f1080dc0677486645b2f09b5dc56f4c3794dd0b8
Ricky Argenbright <honorable.anomaly@gmail.com>
2022-08-18 23:14:54 -0700

    Add emits, move timer
    app.js
    +emits to separate timer calls from animation calls
    
    index.html
    +script for timer.js
    
    timer.js (NEW)
    +moved class Timer

--- Modern-JS/Apps/Timer/app.js
+++ Modern-JS/Apps/Timer/app.js
@@ -1,39 +1,15 @@
-class Timer {
-  constructor(durationInput, startButton, pauseButton) {
-    this.durationInput = durationInput;
-    this.startButton = startButton;
-    this.pauseButton = pauseButton;
-
-    this.startButton.addEventListener("click", this.start);
-    this.pauseButton.addEventListener("click", this.pause);
-  }
-  start = () => {
-    console.log("Starting the timer!");
-    this.tick();
-    this.interval = setInterval(this.tick, 1000);
-  };
-  tick = () => {
-    if (this.timeRemaining <= 0) {
-      this.pause();
-    } else {
-      console.log("Tick!");
-      this.timeRemaining = this.timeRemaining - 1;
-    }
-  };
-  get timeRemaining() {
-    return parseFloat(this.durationInput.value);
-  }
-  set timeRemaining(time) {
-    this.durationInput.value = time;
-  }
-  pause = () => {
-    console.log("Pausing the timer!");
-    clearInterval(this.interval);
-  };
-}
-
 const durationInput = document.querySelector("#duration");
 const startButton = document.querySelector("#start");
 const pauseButton = document.querySelector("#pause");
 
-const timer = new Timer(durationInput, startButton, pauseButton);
+const timer = new Timer(durationInput, startButton, pauseButton, {
+  onStart() {
+    console.log("Timer started!");
+  },
+  onTick() {
+    console.log("Timer ticked!");
+  },
+  onComplete() {
+    console.log("Timer ended!");
+  },
+});
