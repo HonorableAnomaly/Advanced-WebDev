9370c30221259447e6f146d326ab362d7bb712f1
HonorableAnomaly <83691465+HonorableAnomaly@users.noreply.github.com>
2022-08-21 15:12:34 -0700

    Create Modules Demo
    helpers.js
    +3 functions to be exported
    
    index.js
    +3 functions to be imported from helpers.js and called

---
+++ React/ModernReact/modules-demo/src/helpers.js
@@ -0,0 +1,14 @@
+function helpful() {
+  console.log("I did a super helpful thing!");
+}
+
+function deceive() {
+  console.log("These are not the droids you're looking for.");
+}
+
+function fireWhenReady() {
+  console.log("I may fire when ready!");
+}
+
+// export default helpful;
+export { helpful, deceive, fireWhenReady };
---
+++ React/ModernReact/modules-demo/src/index.js
@@ -0,0 +1,12 @@
+// Default import
+// import helpful from "./helpers";
+
+// Import others
+import { helpful, deceive, fireWhenReady } from "./helpers";
+
+// Combined default with others
+// import helpful, { deceive, fireWhenReady } from "./helpers";
+
+helpful();
+deceive();
+fireWhenReady();
