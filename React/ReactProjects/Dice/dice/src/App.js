99e772e6f078eb818b35717e93ca2d41df729a61
Ricky Argenbright <honorable.anomaly@gmail.com>
2022-08-23 15:45:20 -0700

    Create Dice
    RollDice.js
    +RollDice renders Die that randomly chooses a side from defaultProps and triggers 'rolling' when the button is clicked, timing out after 1 second which runs 'shaking' and disables the button
    
    Die.js
    +returns icon with classes
    
    RollDice.css
    +styles for positioning and color
    
    Die.css
    +'shaking' animation from keyframes 'wobble'

---
+++ React/ReactProjects/Dice/dice/src/App.js
@@ -0,0 +1,14 @@
+import Die from "./Die";
+import "./App.css";
+import "./Die.css";
+import RollDice from "./RollDice";
+
+function App() {
+  return (
+    <div className="App">
+      <RollDice />
+    </div>
+  );
+}
+
+export default App;
