01cbc44f2f3712daf524e5352de46e6db18a8c34
Ricky Argenbright <honorable.anomaly@gmail.com>
2022-08-27 15:12:39 -0700

    Refactor Autocomplete for usability
    +rename project
    
    autocomplete.js
    +cut autocomplete function from index.js and pasted here, refactoring for destructured methods
    +refactor querySelectors to root and movie params to item
    
    index.js
    +createAutoComplete object with 4 methods to create instance of reusable autocomplete

--- Adv-WebDev-Projects/CinemaClash/index.js
+++ Adv-WebDev-Projects/CinemaClash/index.js
@@ -1,15 +1,32 @@
-// Reusable debounce
-const debounce = (func, delay = 1000) => {
-  let timeoutId;
-  return (...args) => {
-    if (timeoutId) {
-      clearTimeout(timeoutId);
+createAutoComplete({
+  root: document.querySelector(".autocomplete"),
+  renderOption(movie) {
+    const imgSrc = movie.Poster === "N/A" ? "" : movie.Poster;
+    return `
+    <img src="${imgSrc}" />
+    ${movie.Title} (${movie.Year})`;
+  },
+  onOptionSelect(movie) {
+    onMovieSelect(movie);
+  },
+  inputValue(movie) {
+    return movie.Title;
+  },
+  async fetchData(searchTerm) {
+    const response = await axios.get("http://www.omdbapi.com/", {
+      params: {
+        apikey: "f9f3e425",
+        s: searchTerm,
+      },
+    });
+
+    if (response.data.Error) {
+      return [];
     }
-    timeoutId = setTimeout(() => {
-      func.apply(null, args);
-    }, delay);
-  };
-};
+
+    return response.data.Search;
+  },
+});
 
 const onMovieSelect = async (movie) => {
   const response = await axios.get("http://www.omdbapi.com/", {
