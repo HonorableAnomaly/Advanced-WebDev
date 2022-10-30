7413c7d3b9c22d9fd97345fad0c96fb090b66718
Ricky Argenbright <honorable.anomaly@gmail.com>
2022-10-30 09:43:14 -0700

    Create Yahtzee + Fix Dice lock bug
    Die.js
    +add constructor and props to bind handleIndex
    +add handleIndex method
    // handleIndex passes down handleClick from Dice.js using idx

---
+++ React/ReactProjects/Yahtzee/yahtzee/src/Die.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+import "./Die.css";
+
+class Die extends Component {
+  constructor(props) {
+    super(props);
+
+    this.handleIndex = this.handleIndex.bind(this);
+  }
+  handleIndex() {
+    this.props.handleClick(this.props.idx);
+  }
+  render() {
+    return (
+      <button className={"Die"} style={{ backgroundColor: this.props.locked ? "grey" : "black" }} onClick={this.handleIndex}>
+        {this.props.val}
+      </button>
+    );
+  }
+}
+
+export default Die;
