7ceab0f43485a980edc0cc8882f61668c3e39d91
Ricky Argenbright <honorable.anomaly@gmail.com>
2022-11-07 20:41:26 -0800

    Update styles + Fix bugs
    Dice.js
    +add rolling prop to pass down to Die.js
    
    Die.js
    +add val to defaultProps for starting roll animation
    +add if statement to render to add Die-rolling class if rolling
    
    Game.js
    +add default state rolling set to false
    +add componentDidMount for animateRoll method
    +create animateRoll method that sets the state for rolling to true using a setTimeout
    +add rolling to roll method
    +add if statement to toggleLocked method that checks if the dice are rolling
    +create displayRollInfo method that changes the message on the button based on rollsLeft
    +refactor this.state into variable for render
    +refactor button text to displayRollInfo instead of rollsLeft
    
    ScoreTable.js
    +create getTotalScore method that takes scores and loops through and sums them
    +create an h2 to display getTotalScore

--- React/ReactProjects/Yahtzee/yahtzee/src/ScoreTable.js
+++ React/ReactProjects/Yahtzee/yahtzee/src/ScoreTable.js
@@ -4,6 +4,15 @@ import "./ScoreTable.css";
 import { ones, twos, threes, fours, fives, sixes, threeOfKind, fourOfKind, fullHouse, smallStraight, largeStraight, yahtzee, chance } from "./Rules";
 
 class ScoreTable extends Component {
+  getTotalScore() {
+    const { scores } = this.props;
+    let totalScore = 0;
+    for (let key in scores) {
+      if (scores[key]) totalScore += scores[key];
+    }
+    return totalScore;
+  }
+
   render() {
     const { scores, doScore } = this.props;
 
@@ -101,6 +110,7 @@ class ScoreTable extends Component {
             </tbody>
           </table>
         </section>
+        <h2>TOTAL SCORE: {this.getTotalScore()}</h2>
       </div>
     );
   }
