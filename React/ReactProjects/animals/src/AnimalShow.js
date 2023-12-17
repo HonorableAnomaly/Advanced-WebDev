d7ea129eb50fb6f9e69e381836b13200efdfb731
Ricky Argenbright <honorable.anomaly@gmail.com>
2023-12-16 21:27:13 -0800

    Created Animals App
    +Component (AnimalShow)
       +Props (type, idx)

---
+++ React/ReactProjects/animals/src/AnimalShow.js
@@ -0,0 +1,29 @@
+import bird from "./images/bird.svg";
+import cat from "./images/cat.svg";
+import cow from "./images/cow.svg";
+import dog from "./images/dog.svg";
+import gator from "./images/gator.svg";
+import heart from "./images/heart.svg";
+import horse from "./images/horse.svg";
+
+const svgMap = {
+  bird,
+  cat,
+  cow,
+  dog,
+  gator,
+  horse
+};
+
+function AnimalShow({ type }) {
+  return (
+    <div>
+      <img
+        alt='animal'
+        src={svgMap[type]}
+      />
+    </div>
+  );
+}
+
+export default AnimalShow;
---
+++ React/ReactProjects/animals/src/App.js
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import AnimalShow from "./AnimalShow";
+
+function getRandomAnimal() {
+  const animals = ["bird", "cat", "cow", "dog", "gator", "horse"];
+
+  return animals[Math.floor(Math.random() * animals.length)];
+}
+
+function App() {
+  const [animals, setAnimals] = useState([]);
+
+  const handleClick = () => {
+    setAnimals([...animals, getRandomAnimal()]);
+  };
+
+  const renderedAnimals = animals.map((animal, idx) => {
+    return (
+      <AnimalShow
+        type={animal}
+        key={idx}
+      />
+    );
+  });
+
+  return (
+    <div>
+      <button onClick={handleClick}>Add animal</button>
+      <div>{renderedAnimals}</div>
+    </div>
+  );
+}
+
+export default App;
