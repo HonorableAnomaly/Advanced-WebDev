bd3af8bdd29cff642052eded1e7e33145baae9b6
Ricky Argenbright <honorable.anomaly@gmail.com>
2023-12-21 22:40:42 -0800

    Updated Comps - Accordion
    +Cloned App code for Button.js
    
    +Accordion Component
      +Mapping accordion items through expanded parameters that manage click listener states
      +Displaying with TailwindCSS and imported icons

--- React/ReactProjects/comps/src/App.js
+++ React/ReactProjects/comps/src/App.js
@@ -1,41 +1,25 @@
-import { GoVerified, GoCloud, GoSquirrel, GoAlert, GoXCircle } from "react-icons/go";
-import Button from "./Button";
+import Accordion from "./components/Accordion";
 
 function App() {
-  return (
-    <div>
-      <div>
-        <Button primary>
-          <GoVerified />
-          Click Me!
-        </Button>
-      </div>
-      <div>
-        <Button secondary>
-          <GoCloud />
-          Buy Now!
-        </Button>
-      </div>
-      <div>
-        <Button success>
-          <GoSquirrel />
-          See Deal!
-        </Button>
-      </div>
-      <div>
-        <Button warning>
-          <GoAlert />
-          Hide Ads!
-        </Button>
-      </div>
-      <div>
-        <Button danger>
-          <GoXCircle />
-          Update!
-        </Button>
-      </div>
-    </div>
-  );
+  const items = [
+    {
+      key: "g5252ff4",
+      label: "Can I haz cheezburgr?",
+      content: "Yes, of course you may! Yes, of course you may! Yes, of course you may! Yes, of course you may! Yes, of course you may! Yes, of course you may!"
+    },
+    {
+      key: "bh47uyj3h",
+      label: "Can I also haz donutz",
+      content:
+        "After you eat your dinner! After you eat your dinner! After you eat your dinner! After you eat your dinner! After you eat your dinner! After you eat your dinner!"
+    },
+    {
+      key: "gav234f2g",
+      label: "Can I also also haz hugz?",
+      content: "Why did you not say so?! Why did you not say so?! Why did you not say so?! Why did you not say so?! Why did you not say so?! Why did you not say so?!"
+    }
+  ];
+  return <Accordion items={items} />;
 }
 
 export default App;
---
+++ React/ReactProjects/comps/src/pages/ButtonPage.js
@@ -0,0 +1,41 @@
+import { GoVerified, GoCloud, GoSquirrel, GoAlert, GoXCircle } from "react-icons/go";
+import Button from "../components/Button";
+
+function ButtonPage() {
+  return (
+    <div>
+      <div>
+        <Button primary>
+          <GoVerified />
+          Click Me!
+        </Button>
+      </div>
+      <div>
+        <Button secondary>
+          <GoCloud />
+          Buy Now!
+        </Button>
+      </div>
+      <div>
+        <Button success>
+          <GoSquirrel />
+          See Deal!
+        </Button>
+      </div>
+      <div>
+        <Button warning>
+          <GoAlert />
+          Hide Ads!
+        </Button>
+      </div>
+      <div>
+        <Button danger>
+          <GoXCircle />
+          Update!
+        </Button>
+      </div>
+    </div>
+  );
+}
+
+export default ButtonPage;
