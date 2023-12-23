4e35d1813ec2a48dfef959f782b2c077807800c9
Ricky Argenbright <honorable.anomaly@gmail.com>
2023-12-22 23:01:12 -0800

    Updated Comps - Link, Sidebar, Routes, DropdownPage, navigation
    * Imported and implemented Routes to App.js
    
    * Created Link.js to control navigation within the app
    
    * Created Routes.js to implement Routes on App.js
    
    * Created Sidebar.js to list Links to DropdownPage.js, AccordionPage.js and ButtonPage.js
    
    * Created navigation.js to utilize createContext and wrap the App in a Provider for navigation
    
    * Created use-navigation.js hook for reusable NavigationContext
    
    * Wrapped App with NavigationProvider in index.js
    
    * Created DropdownPage.js and copied over code from App.js, refactored as its own Page

--- React/ReactProjects/comps/src/App.js
+++ React/ReactProjects/comps/src/App.js
@@ -1,25 +1,25 @@
-import { useState } from "react";
-import Dropdown from "./components/Dropdown";
+import Route from "./components/Routes";
+import AccordionPage from "./pages/AccordionPage";
+import DropdownPage from "./pages/DropdownPage";
+import ButtonPage from "./pages/ButtonPage";
+import Sidebar from "./components/Sidebar";
 
 function App() {
-  const [selection, setSelection] = useState(null);
-
-  const handleSelect = (option) => {
-    setSelection(option);
-  };
-
-  const options = [
-    { label: "Red", value: "red" },
-    { label: "Green", value: "green" },
-    { label: "Blue", value: "blue" }
-  ];
-
   return (
-    <Dropdown
-      options={options}
-      value={selection}
-      onChange={handleSelect}
-    />
+    <div className='container mx-auto grid grid-cols-6 gap-4 mt-4'>
+      <Sidebar />
+      <div className='col-span-5'>
+        <Route path='/'>
+          <DropdownPage />
+        </Route>
+        <Route path='/accordion'>
+          <AccordionPage />
+        </Route>
+        <Route path='/buttons'>
+          <ButtonPage />
+        </Route>
+      </div>
+    </div>
   );
 }
 
