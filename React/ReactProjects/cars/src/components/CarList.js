25f652f376be1ce235c590fd13253bf42062edc1
Ricky Argenbright <honorable.anomaly@gmail.com>
2023-12-29 19:36:58 -0800

    Updated cars - CarList, CarSearch, CarValue, styles
    * Added classNames to wrapping div in App
    
    * In CarList.js, employed useSelector to filter cars, handleCarDelete to remove cars and renderedCars to display the list of cars after being mapped
    
    * In CarValue.js, employed useSelector to filter cars data to add costs together of displayed cars
    
    * Imported styles from CSS file and bulma in index.js
    
    * Created extraReducers in formSlice.js to use addCar from carsSlice.js to reset name and cost fields

--- React/ReactProjects/cars/src/components/CarList.js
+++ React/ReactProjects/cars/src/components/CarList.js
@@ -1,11 +1,48 @@
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { removeCar } from "../store";
 
 function CarList() {
-  const cars = useSelector((state) => {
-    return state.cars.data;
+  const dispatch = useDispatch();
+  const { cars, name } = useSelector(({ form, cars: { data, searchTerm } }) => {
+    const filteredCars = data.filter((car) => car.name.toLowerCase().includes(searchTerm.toLowerCase()));
+
+    return {
+      cars: filteredCars,
+      name: form.name
+    };
   });
 
-  return <div></div>;
+  const handleCarDelete = (car) => {
+    dispatch(removeCar(car.id));
+  };
+
+  const renderedCars = cars.map((car) => {
+    const bold = name && car.name.toLowerCase().includes(name.toLowerCase());
+
+    return (
+      <div
+        key={car.id}
+        className={`panel ${bold && "bold"}`}
+      >
+        <p>
+          {car.name} - ${car.cost}
+        </p>
+        <button
+          className='button is-danger'
+          onClick={() => handleCarDelete(car)}
+        >
+          Delete
+        </button>
+      </div>
+    );
+  });
+
+  return (
+    <div className='car-list'>
+      {renderedCars}
+      <hr />
+    </div>
+  );
 }
 
 export default CarList;
--- React/ReactProjects/cars/src/components/CarValue.js
+++ React/ReactProjects/cars/src/components/CarValue.js
@@ -1,5 +1,11 @@
+import { useSelector } from "react-redux";
+
 function CarValue() {
-  return <div>CarValue</div>;
+  const totalCost = useSelector(({ cars: { data, seachTerm } }) =>
+    data.filter((car) => car.name.toLowerCase().includes(seachTerm.toLowerCase())).reduce((acc, car) => acc + car.cost, 0)
+  );
+
+  return <div className='car-value'>Total Cost: ${totalCost}</div>;
 }
 
 export default CarValue;
